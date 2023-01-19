d1bd85473a800339cd377c796236c8169849eb89
Siva Prabhaharan Jayakumar <siva_prabhaharan_jayakumar@epam.com>
2023-01-19 17:39:38 +0530

    Added Vercel Controller

---
+++ VercelApi/VercelApi/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VercelApi.Controllers
+{
+    [Route("vercel")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestEndpoint()
+        {
+            return Ok("Success Response");
+        }
+    }
+}
